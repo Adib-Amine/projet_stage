e29205693aa55619309cbade5bca8433d566b4ea
Adib-Amine <amineadib007@gmail.com>
2020-10-06 13:23:26 +0100

    add header and bootstrap

---
+++ src/components/layout/Header.vue
@@ -0,0 +1,22 @@
+<template>
+    <nav class="navbar navbar-dark bg-primary navbar-expand-md">
+        <div class="container-fluid">
+        <a class="navbar-brand" href="#">TimeTable</a>
+        <div class="collapse navbar-collapse" id="navcol-1">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item"><a class="nav-link " href="#">LogOut</a></li>
+                </ul>
+            </div>
+        </div>    
+      </nav>
+</template>
+
+<script>
+export default {
+    name:"Header"    
+}
+</script>
+
+<style scoped>
+
+</style>
