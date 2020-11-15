3b109c7f6ff5524e66cd360426a4c2e4fed8f718
Adib-Amine <amineadib007@gmail.com>
2020-11-15 21:19:09 +0100

    add dynamique table

--- src/views/Crud_Emploi.vue
+++ src/views/Crud_Emploi.vue
@@ -1,174 +1,56 @@
 <template>
     <div class="container">
+        <b-dropdown :text="Text" block variant="primary" class="m-2" menu-class="w-100">
         <div v-for="filier in this.info.data" :key="filier.id">
-        <b-dropdown 
-            text="Select Filier" block variant="primary" class="m-2" menu-class="w-100"
-            >
-            <b-dropdown-item href="#">{{filier.title}}</b-dropdown-item>
-            <b-dropdown-item href="#">BDCC1</b-dropdown-item>
-            <b-dropdown-item href="#">BDCC2</b-dropdown-item>
-            <b-dropdown-item href="#" disabled>GLSID1</b-dropdown-item>
-            <b-dropdown-item href="#">GLSID2</b-dropdown-item>
-        </b-dropdown>
+            <b-dropdown-item v-on:click="selectedFilier(filier)">
+                {{filier.title}}
+            </b-dropdown-item>
         </div>
+        </b-dropdown>
         <div class="row">
-        <div class="Table">
-             <div class="Title">
-                    <div>BDCC</div>
-                </div>
+            <div class="Table">
+            <div class="Title">
+                    <br><div>{{filier_title}}</div><br>
+            </div>
             <div class="Heading">
                 <div class="EmptyCell col-xs">
                     <div class="box"></div>
                 </div>
-                <div class="Cell col-xs">
+                <div class="CellDays col-xs">
                     <div class="box">8h30 à 10h30</div>
                 </div>
-                <div class="Cell col-xs">
+                <div class="CellDays col-xs">
                     <div class="box">10h30 à 12h30</div>
                 </div>
                 <div class="EmptyCell col-xs">
                     <div class="box"></div>
                 </div>
-                <div class="Cell col-xs">
+                <div class="CellDays col-xs">
                     <div class="box">14h à 16h</div>
                 </div>
-                <div class="Cell col-xs">
+                <div class="CellDays col-xs">
                     <div class="box">16h à 18h </div>
                 </div>
             </div>
-            <div class="Row">
-                <div class="Cell   col-xs">
-                    <div class="box">Lundi</div>
+
+            <div class="Row" v-for="j in 6" :key="j">
+                <div class="CellDays col-xs">
+                    <div class="box">{{Days[j-1]}}</div>
                 </div>
-                <div class="Cell col-xs">
-                    <div class="box">
-                        <div> Programmation Orientée Objet Java </div>
-                        <div> (48) </div> 
-                        <div>M. NAJI </div>
-                        <div> S. 74 </div>
+                    <div class="col-xs" :class="classCell(i)" v-for="i in 5" :key="i">
+                    <div class="box" v-if="i != 3">
+                        {{module.title}} <br>
+                        {{module.nbr}} <br>
+                        {{module.prof}} <br>
+                        {{module.salle}}
                     </div>
-                </div>
-                <div class="Cell col-xs">
-                    <div> Programmation Orientée Objet Java </div>
-                    <div> (48) </div> 
-                    <div>M. NAJI </div>
-                    <div> S. 74 </div>
-                </div>
-                <div class="EmptyCell col-xs">
-                    <div class="box"></div>
-                </div>
-                <div class="Cell col-xs">
-                    <div class="box">Programmation Orientée Objet <br>Java (48) <br> M. NAJI <br>S. 74</div>
-                </div>
-                <div class="Cell col-xs">
-                    <div class="box">Gestion de la <br> production et de la <br> qualité (28) <br> M. JAMMOU</div>
-                </div>
+                    <div class="box" v-else>
+                    </div>
+                    </div>
             </div>
-            <div class="Row">
-                <div class="Cell   col-xs">
-                    <div class="box">Mardi</div>
-                </div>
-                <div class="Cell col-xs">
-                    <div> Programmation Orientée Objet Java </div>
-                    <div> (48) </div> 
-                    <div>M. NAJI </div>
-                    <div> S. 74 </div>
-                </div>
-                <div class="Cell col-xs">
-                    <div class="box">Programmation Orientée Objet <br>Java (48) <br> M. NAJI <br>S. 74</div>
-                </div>
-                <div class="EmptyCell col-xs">
-                    <div class="box"></div>
-                </div>
-                <div class="Cell col-xs">
-                    <div class="box">Programmation Orientée Objet <br>Java (48) <br> M. NAJI <br>S. 74</div>
-                </div>
-                <div class="Cell col-xs">
-                    <div class="box">Programmation Orientée Objet <br>Java (48) <br> M. NAJI <br>S. 74</div>
-                </div>
-            </div>
-            <div class="Row">
-                <div class="Cell   col-xs">
-                    <div class="box">Mercredi</div>
-                </div>
-                <div class="Cell col-xs">
-                    <div class="box">Programmation Orientée Objet <br>Java (48) <br> M. NAJI <br>S. 74</div>
-                </div>
-                <div class="Cell col-xs">
-                    <div class="box">Programmation Orientée Objet <br>Java (48) <br> M. NAJI <br>S. 74</div>
-                </div>
-                <div class="EmptyCell col-xs">
-                    <div class="box"></div>
-                </div>
-                <div class="Cell col-xs">
-                    <div class="box">Programmation Orientée Objet <br>Java (48) <br> M. NAJI <br>S. 74</div>
-                </div>
-                <div class="Cell col-xs">
-                    <div class="box">Programmation Orientée Objet <br>Java (48) <br> M. NAJI <br>S. 74</div>
-                </div>
-            </div>
-            <div class="Row">
-                <div class="Cell   col-xs">
-                    <div class="box">Jeudi</div>
-                </div>
-                <div class="Cell col-xs">
-                    <div class="box">Programmation Orientée Objet <br>Java (48) <br> M. NAJI <br>S. 74</div>
-                </div>
-                <div class="Cell col-xs">
-                    <div class="box">Programmation Orientée Objet <br>Java (48) <br> M. NAJI <br>S. 74</div>
-                </div>
-                <div class="EmptyCell col-xs">
-                    <div class="box"></div>
-                </div>
-                <div class="Cell col-xs">
-                    <div class="box">Programmation Orientée Objet <br>Java (48) <br> M. NAJI <br>S. 74</div>
-                </div>
-                <div class="Cell col-xs">
-                    <div class="box">Programmation Orientée Objet <br>Java (48) <br> M. NAJI <br>S. 74</div>
-                </div>
-            </div>
-            <div class="Row">
-                <div class="Cell   col-xs">
-                    <div class="box">Vendredi</div>
-                </div>
-                <div class="Cell col-xs">
-                    <div class="box">Programmation Orientée Objet <br>Java (48) <br> M. NAJI <br>S. 74</div>
-                </div>
-                <div class="Cell col-xs">
-                    <div class="box">Programmation Orientée Objet <br>Java (48) <br> M. NAJI <br>S. 74</div>
-                </div>
-                <div class="EmptyCell col-xs">
-                    <div class="box"></div>
-                </div>
-                <div class="Cell col-xs">
-                    <div class="box">Programmation Orientée Objet <br>Java (48) <br> M. NAJI <br>S. 74</div>
-                </div>
-                <div class="Cell col-xs">
-                    <div class="box">Programmation Orientée Objet <br>Java (48) <br> M. NAJI <br>S. 74</div>
-                </div>
-            </div>
-            <div class="Row">
-                <div class="Cell   col-xs">
-                    <div class="box">Samedi</div>
-                </div>
-                <div class="Cell col-xs">
-                    <div class="box">Programmation Orientée Objet <br>Java (48) <br> M. NAJI <br>S. 74</div>
-                </div>
-                <div class="Cell col-xs">
-                    <div class="box">Programmation Orientée Objet <br>Java (48) <br> M. NAJI <br>S. 74</div>
-                </div>
-                <div class="EmptyCell col-xs">
-                    <div class="box"></div>
-                </div>
-                <div class="Cell col-xs">
-                    <div class="box">Programmation Orientée Objet <br>Java (48) <br> M. NAJI <br>S. 74</div>
-                </div>
-                <div class="Cell col-xs">
-                    <div class="box">Programmation Orientée Objet <br>Java (48) <br> M. NAJI <br>S. 74</div>
-                </div>
-            </div>
+
         </div>
-    </div>
+        </div>
     </div>
 </template>
 
@@ -179,6 +61,17 @@ export default {
     data(){
         return{
             info : "",
+            filier_id : null,
+            filier_label : null,
+            filier_title : null,
+            Days : ["Lundi","Mardi","Mercredi","Jeudi","Vendredi","Samedi"],
+            cellStatus : true,
+            module : {
+                title : "ogrammation Orientée Objet Java",
+                nbr : "48",
+                prof : "M. NAJI",
+                salle : "75"
+            }
         }
     },
     methods : {
@@ -187,13 +80,31 @@ export default {
             .get("http://localhost:8000/filiers")
             .then(response => (this.info = response))
         },
+        selectedFilier(filier){
+            this.filier_id = filier.id
+            this.filier_label = filier.label
+            this.filier_title = filier.title
+        },
+        classCell(ind){
+            if(ind != 3)
+                return "Cell"
+            return "EmptyCell"
+        }
     },
     mounted() {
         this.fetchData()
     // this.$root.$on('bv::dropdown::show', bvEvent => {
     //   console.log('Dropdown is about to be shown', bvEvent)
     // })
-  }
+    },
+    computed : {
+        Text(){
+            if(this.filier_id)
+                return this.filier_label
+            return "Select Filier"
+        },
+        
+    }
 }
 </script>
 
@@ -203,48 +114,53 @@ export default {
     width: auto;
 }
 .Title
-    {
-        display: table-caption;
-        text-align: center;
-        font-weight: bold;
-        font-size: larger;
-    }
+{
+    display: table-caption;
+    text-align: center;
+    font-weight: bold;
+    font-size: larger;
+}
 .Table
-    {
-        display: table;
-    }
-    .Heading
-    {
-        display: table-row;
-        font-weight: bold;
-        text-align: center;
-    }
-    .Row
-    {
-        display: table-row;
-        min-width: 100px;
-        height:120px;
-    }
-    .Cell
-    {
-        display: table-cell;
-        border: solid;
-        border-width: thin;
-        /* padding-left: 5px; */
-        /* padding-right: 5px; */
-        height: 80px;
-        text-align: center;
-        margin: auto;
-        min-width: 120px;
-    }
-    /* .box {
-        margin: auto;
-    } */
-    .EmptyCell{
-        display: table-cell;
-        width: 5px;
-    }
-    .HeaderTable{
-        display:block;
-    }
+{
+    display: table;
+}
+.Heading
+{
+    display: table-row;
+    font-weight: bold;
+    text-align: center;
+}
+.Row
+{
+    display: table-row;
+    min-width: 100px;
+    height:100px;
+}
+.Cell
+{
+    display: table-cell;
+    border: solid;
+    border-width: thin;
+    height: 80px;
+    text-align: center;
+    margin: auto;
+    min-width: 260px;
+}
+.Cell:hover{
+    background-color: #dee2e6;
+}
+.CellDays{
+    display: table-cell;
+    border: solid;
+    border-width: thin;
+    height: 50px;
+    text-align: center;
+    margin: auto;
+    min-width: 100px;
+    vertical-align: middle
+}
+.EmptyCell{
+    display: table-cell;
+    min-width: 20px;
+}
 </style>
