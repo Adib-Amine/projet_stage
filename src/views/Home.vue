7254e6e50a7f9ada84879e9db27119da0ed994b5
Adib-Amine <amineadib007@gmail.com>
2021-01-05 00:02:44 +0100

    add home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,16 @@
 <template>
   <b-container fluid>
-    <!-- <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800,900" rel="stylesheet"> -->
-    <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"> -->
-    <!-- <link rel="preconnect" href="https://fonts.gstatic.com"> -->
     <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
     <b-row>
       <b-col id="description">
         <p id="title">Avec ClassTime</p>
-        <span><i class='bx bx-calendar-check'></i> Visualisez vos emplois du temps avec toute
+        <span><i class='bx bx-calendar-check bx-burst' style='color:#0341FF'></i> Visualisez vos emplois du temps avec toute
           aisance.<br>
-        </span> <span><i class='bx bx-calendar-check'></i> Modifiez vos créneaux avec une
+        </span> <span><i class='bx bx-calendar-check bx-burst' style='color:#0341FF' ></i> Modifiez vos créneaux avec une
           flexibilité.<br>
-        </span> <span><i class='bx bx-calendar-check'></i> Restez toujours notifiés à l’approche
+        </span> <span><i class='bx bx-calendar-check bx-burst' style='color:#0341FF'></i> Restez toujours notifiés à l’approche
           des devoirs et d’examens.
         </span>
-
-
       </b-col>
       <b-col cols="6" id="img-col">
         <img  id="img" src="../assets/Time.png" alt="Fluid-grow image"/>
@@ -28,7 +23,6 @@
 <style>
 #img-col {
   display: flex;
-  /* justify-content: start; */
   align-items: center;
 }
 
